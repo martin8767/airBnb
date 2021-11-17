89246fd433467f9fca836455d99d9a524b78349f
Martin Rucci <martin8767@github.com>
2021-11-17 13:42:13 -0300

    users controller, view and update profile

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,10 +3,27 @@ module Api
     class ApiController < ActionController::API
       include DeviseTokenAuth::Concerns::SetUserByToken
     
-      rescue_from ActiveRecord::RecordNotFound, with: :not_found
+      rescue_from ActionController::ParameterMissing,   with: :render_parameter_missing
+      rescue_from ActiveRecord::RecordInvalid,          with: :render_record_invalid
+      rescue_from ActiveRecord::RecordNotFound,         with: :not_found
+      rescue_from ActiveRecord::RecordNotUnique,        with: :not_unique
     
+      private
+
+      def render_parameter_missing(e)
+        render json: { error: e.message }, status: :unprocessable_entity
+      end
+
+      def render_record_invalid(e)
+        render json: { error: e.message }, status: :bad_recuest
+      end
+
       def not_found(e)
         render json: { error: e.message }, status: :not_found
+      end
+
+      def not_unique(e)
+        render json: { error: e.message }, status: :conflict
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   mount_devise_token_auth_for 'User', at: '/api/v1/users', controllers: {
-    confirmations:      'api/v1/confirmations',
+    # confirmations:      'api/v1/confirmations',
     passwords:          'api/v1/passwords',
     registrations:      'api/v1/registrations',
     sessions:           'api/v1/sessions'
